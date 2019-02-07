469a40b01f2ea03d6e152a2f8acec198fd7aeae1
Xua <williamblakelock.wb@gmail.com>
2019-02-08 09:51:43 +1300

    Create config_example.py

---
+++ config_example.py
@@ -0,0 +1,10 @@
+TOKEN = "token"
+PREFIX = "your prefix"
+EXTENSIONS = ["list", "of", "startup", "extensions"]
+REDIS_ADDRESS = "redis host address"
+ASYNCPG = {
+    "user": "postgresql username",
+    "password": "postgresql password",
+    "database": "database name",
+    "host": "host address"
+}
