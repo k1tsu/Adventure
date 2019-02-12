9431b062b861130860c5c40f01e0ba2a61425482
XuaTheGrate <williamblakelock.wb@gmail.com>
2019-02-12 13:54:10 +1300

    finalize pink blobs

--- blobs.py
+++ blobs.py
@@ -1,14 +1,14 @@
 from discord import PartialEmoji
 
 
-BLOB_SALUTE = PartialEmoji(False, "blobsalute", 543272198463553537)
+BLOB_SALUTE = PartialEmoji(False, "pinkblobsalute", 544625129997598733)
 BLOB_THINK = PartialEmoji(False, "pinkblobthinks", 511626506049683458)
-BLOB_TICK = PartialEmoji(True, "blobTick", 543325591542169600)
-BLOB_CROSS = PartialEmoji(True, "blobCross", 543325636484136970)
+BLOB_TICK = PartialEmoji(False, "pinkblobyes", 544628885153906698)
+BLOB_CROSS = PartialEmoji(False, "pinkblobdeny", 544628885258633216)
 BLOB_PLSNO = PartialEmoji(False, "pinkblobplsno", 511668250313097246)
-BLOB_WINK = PartialEmoji(False, "blobwink", 543332401217142793)
+BLOB_WINK = PartialEmoji(False, "pinkblobwink", 544628885023621126)
 BLOB_PARTY = PartialEmoji(True, "partyblob", 543542441018654732)
 BLOB_O = PartialEmoji(False, "pinkbolb", 511660130195210263)
 BLOB_THUMB = PartialEmoji(False, "pinkblobthumbsup", 511649152669843477)
 BLOB_ARMSCROSSED = PartialEmoji(False, "pinkblobcrossedarms", 512477512719400970)
-BLOB_ANGERY = PartialEmoji(False, "blobangery", 544391986313035787)
+BLOB_ANGERY = PartialEmoji(False, "pinkblobangery", 544633192951316510)
