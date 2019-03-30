54bd7b3643a36af0367f08e7f6496431a1a56295
Xua Yraili <williamblakelock.wb@gmail.com>
2019-03-30 18:40:53 +1300

    kill me

--- cogs/support.py
+++ cogs/support.py
@@ -7,9 +7,9 @@ from discord.ext import commands
 class Support(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.guild: discord.Guild = None
-        self.dehoist_role: discord.Role = None
-        self.member_role: discord.Role = None
+        self.guild = self.bot.get_guild(561390061963182132)
+        self.dehoist_role = self.guild.get_role(561405074585157693)
+        self.member_role = self.guild.get_role(561404610070183936)
         self.dehoist_task: asyncio.Task = self.bot.loop.create_task(self.dehoisting())
 
     def cog_unload(self):
